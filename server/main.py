b8d68c3844cd57718cf26af52fe48c074340e995
Kervin <kervin.silaire@gmail.com>
2022-12-27 00:00:29 -0500

    removed comments

--- server/main.py
+++ server/main.py
@@ -2,11 +2,9 @@ from server.gateways.data.router import setup_data_layer
 from flask import Flask
 from flask_cors import CORS
 
+
 class ConsensusNode:
     app = Flask(__name__)
-    server_is_alive = False
-    server = None
-    # host = '0.0.0.0'
     host = 'localhost'
     port = 8080
 
@@ -20,7 +18,6 @@ class ConsensusNode:
         self.addCORS()
         data_layer = setup_data_layer()
         self.app.register_blueprint(data_layer)
-        # self.app.register_blueprint(chain_layer)
 
     def start(self):
         self.app.run(host=self.host, port=self.port, debug=True)
